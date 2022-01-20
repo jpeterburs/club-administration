b1b8180f6975982df60933da4b0fd8bbf30645e5
jpeterburs <j.peterburs@outlook.de>
2022-01-20 12:55:20 +0000

    Add vaildators for member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,11 @@
 class Member < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :date_of_birth, presence: true
+  validates :occupation, presence: true
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
 end
