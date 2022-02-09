199bffd5474285b8bba47f6833b0a01bf032abe5
EduardRempel <eduardrempel@gmx.de>
2022-02-09 21:23:03 +0100

    Update app/models/bank_transaction.rb
    Co-authored-by: Jonas <60157522+jpeterburs@users.noreply.github.com>

--- app/models/bank_transaction.rb
+++ app/models/bank_transaction.rb
@@ -1,4 +1,5 @@
 class BankTransaction < ApplicationRecord
-    def self.get_transactions_for_time_period(date_from, date_to)
-    end
+  def self.for_time_period(start_date, end_date)
+    where('transaction_time >= ? AND transaction_time <= ?', start_date, end_date)
+  end
 end
