da1b7921ec2496a480a9c08d61c607109a0e8f50
Eduard Rempel <EduardRempel@gmx.de>
2022-02-09 01:18:54 +0100

    added annual balance to balance_controller

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -3,12 +3,14 @@ class BalancesController < ApplicationController
     if params[:date_from].blank? and params[:data_to].blank?
       @transactions = BankTransaction.all
     elsif !params[:data_from].blank? and !params[:data_to].blank?
-      @transactions = BankTransaction.get_transactions_for_time_period
+      @transactions = BankTransaction.get_transactions_for_time_period(params[:data_from], params[:data_to])
     else
       render :nothing => true, :status => :bad_request 
     end
   end
   
   def annual_balance
+    @passiva = Balance.passive()
+    @activa =Balance.active()
   end
 end
