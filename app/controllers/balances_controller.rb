cd4a0b9fa7af52bfe8d39d4b44f47310c2b1ef89
jpeterburs <j.peterburs@outlook.de>
2022-02-10 09:19:37 +0000

    Finish balance rendering

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -3,10 +3,13 @@ class BalancesController < ApplicationController
   end
 
   def result
-    return redirect_to(balances_new_path) unless params[:balance][:start_date] && params[:balance][:end_date]
-
-    @transactions = BankTransaction.for_time_period(params[:start_date], params[:end_date])
+    @transactions = if params[:balance][:start_date].present? && params[:balance][:end_date].present?
+                      BankTransaction.for_time_period(params[:start_date], params[:end_date])
+                    else
+                      BankTransaction.all
+                    end
     @balance = Balance.new(@transactions, nil) # TODO add bank account
-    # TODO something is still missing here
+
+    render turbo_stream: turbo_stream.update('result', partial: 'result')
   end
 end
